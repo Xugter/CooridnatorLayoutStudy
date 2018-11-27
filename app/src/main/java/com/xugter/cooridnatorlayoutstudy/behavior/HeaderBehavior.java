1b17c151ac10d978f39008257ad3527f3839b67b
Xugter <xubomain@163.com>
2018-11-27 16:06:26 +0800

    prepare

---
+++ app/src/main/java/com/xugter/cooridnatorlayoutstudy/behavior/HeaderBehavior.java
@@ -0,0 +1,66 @@
+package com.xugter.cooridnatorlayoutstudy.behavior;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.widget.NestedScrollView;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+public class HeaderBehavior extends CoordinatorLayout.Behavior<View> {
+
+    private final static String TAG = "====HeaderBehavior====";
+
+    public HeaderBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean layoutDependsOn(@NonNull CoordinatorLayout parent, @NonNull View child, @NonNull View dependency) {
+        Log.d(TAG, "layoutDependsOn");
+        return dependency instanceof NestedScrollView;
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
+        Log.d(TAG, "onStartNestedScroll");
+        return true;
+    }
+
+    @Override
+    public void onNestedScrollAccepted(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
+        Log.d(TAG, "onNestedScrollAccepted");
+        super.onNestedScrollAccepted(coordinatorLayout, child, directTargetChild, target, axes, type);
+    }
+
+    @Override
+    public void onNestedPreScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
+        Log.d(TAG, "onNestedPreScroll");
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type);
+    }
+
+    @Override
+    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
+        Log.d(TAG, "onNestedScroll");
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type);
+    }
+
+    @Override
+    public void onStopNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View target, int type) {
+        Log.d(TAG, "onStopNestedScroll");
+        super.onStopNestedScroll(coordinatorLayout, child, target, type);
+    }
+
+    @Override
+    public boolean onNestedPreFling(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View target, float velocityX, float velocityY) {
+        Log.d(TAG, "onNestedPreFling");
+        return super.onNestedPreFling(coordinatorLayout, child, target, velocityX, velocityY);
+    }
+
+    @Override
+    public boolean onNestedFling(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View target, float velocityX, float velocityY, boolean consumed) {
+        Log.d(TAG, "onNestedFling");
+        return super.onNestedFling(coordinatorLayout, child, target, velocityX, velocityY, consumed);
+    }
+}
