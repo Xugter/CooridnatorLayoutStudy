96f616ab82c530a539d30c8b858823c98a47be2b
Xugter <xubomain@163.com>
2018-11-27 19:33:50 +0800

    prepare

--- app/src/main/java/com/xugter/cooridnatorlayoutstudy/MainActivity.java
+++ app/src/main/java/com/xugter/cooridnatorlayoutstudy/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.xugter.cooridnatorlayoutstudy.first.FirstActivity;
 import com.xugter.cooridnatorlayoutstudy.simpletitle.SimpleTitleActivity;
 import com.xugter.cooridnatorlayoutstudy.test.TestActivity;
 
@@ -22,5 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void test(View view) {
         startActivity(new Intent(this, TestActivity.class));
+    }
+
+    public void first(View view) {
+        startActivity(new Intent(this, FirstActivity.class));
     }
 }
---
+++ app/src/main/java/com/xugter/cooridnatorlayoutstudy/first/FirstBehavior.java
@@ -0,0 +1,13 @@
+package com.xugter.cooridnatorlayoutstudy.first;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class FirstBehavior extends CoordinatorLayout.Behavior<View> {
+
+    public FirstBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
